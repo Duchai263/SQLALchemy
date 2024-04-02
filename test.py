a04c61da883bf8906e0cb5647de5549bb6a3799a
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2024-04-03 00:14:39 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import re 
-
-df = pd.read_csv("store-inventory/inventory.csv")
-df['product_price'] = df["product_price"].apply(lambda x: re.sub(r'[^0-9]','', x))
-# print(df["product_price"])
-
-print(df.keys())
